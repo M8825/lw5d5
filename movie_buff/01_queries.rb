023920af083932ae68da066bebfc388a3f023ed9
lynsiearagon <lynsie.aragon@gmail.com>
2022-12-09 15:19:19 -0500

    harrison_ford complete

--- movie_buff/01_queries.rb
+++ movie_buff/01_queries.rb
@@ -22,7 +22,10 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
-  Movie.joins(:actors).where.not(castings: { ord: 1 }).select(:id, :title)
+  # Actor.select(:name, :ord).joins(:castings).where("actors.name = 'Harrison Ford'")
+
+  Movie.select(:id, :title).joins(:actors).where(actors: {name: "Harrison Ford"} ).where.not(castings: {ord: 1} )
+
 end
 
 def biggest_cast
@@ -39,6 +42,8 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the largest casts (i.e., most
   # actors).
+
+
   
 end
 
